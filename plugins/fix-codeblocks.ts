2ca19697704018152bddbced248dfd2fa2c51599
nzbr <mail@nzbr.de>
2024-01-07 19:56:20 +0100

    fix-codeblocks: fix for mermaid

--- plugins/fix-codeblocks.ts
+++ plugins/fix-codeblocks.ts
@@ -4,6 +4,9 @@ export const name = "fix-codeblocks";
 
 // Fix code blocks
 export function fn({ $ }: StepInputs): void {
+  $('pre.mermaid').each((_, el) => {
+    $(el).html($(el).text());
+  });
   $('pre > code').each((_, el) => {
     const lines = ($(el).html() ?? '').split('\n');
     for (let i = 1; i < lines.length; i++) {
